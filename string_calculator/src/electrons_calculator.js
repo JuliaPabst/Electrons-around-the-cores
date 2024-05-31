31c013fe7e5e990917a84ed6cadf539398386fb2
Jules <testadmin@Julias-MacBook-Pro-2.local>
2024-05-31 14:05:38 +0200

    2 - write prod code:  Andrei Brate: observer, Julia Pabst: driver

--- string_calculator/src/electrons_calculator.js
+++ string_calculator/src/electrons_calculator.js
@@ -4,18 +4,21 @@ ElectronsCalculator.prototype.calculate = function(array) {
 let result = 0;
 if(array.length < 4) {
 	return -1;
-} 
-
-for(let i = 0; i < array.length; i++) {
-	if(array[i] == 3) {
-		result += 2;
-	} else if(array[i] == 5) {
-		result += 4;
-	} else {
-		result += 0;
+} else if(array.length > 6) {
+	return -2;
+} else {
+	for(let i = 0; i < array.length; i++) {
+		if(array[i] == 3) {
+			result += 2;
+		} else if(array[i] == 5) {
+			result += 4;
+		} else {
+			result += 0;
+		}
 	}
+		return result;
 }
-	return result;
+
 };
 
 
