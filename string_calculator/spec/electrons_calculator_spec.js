3dd5659a28620ae756a7a356566652b8f432a5c7
Jules <testadmin@Julias-MacBook-Pro-2.local>
2024-05-31 13:59:59 +0200

    adapt to do list

--- string_calculator/spec/electrons_calculator_spec.js
+++ string_calculator/spec/electrons_calculator_spec.js
@@ -1,12 +1,17 @@
 /*
 
 TODO-LIST:
-- get [] as input - excpect 0 as output
-- get [3] as input - expect 2 as output
-- get [5] as input - expect 4 as output
-- get [1,2] as input - expect 0 as output
-- get [1,3] as input - expect 2 as output
-- get [3,5] as input - expect 6 as output
+if it is less than 4 - should return -1
+if it is more than 6 - should return -2
+
+- get [] as input - excpect -1 as output
+- get [3] as input - expect -1 as output
+- get [5] as input - expect -1 as output
+- get [1,2] as input - expect -1 as output
+- get [1,3] as input - expect -1 as output
+- get [3,5] as input - expect -1 as output
+- get [1,2,3,4] as input - expect 2 as output
+- get [1,2,3,4,5,6,7] as input - expect -2 as output
 
 1 - write failing test:
 2 - write prod code:
@@ -38,9 +43,5 @@ describe('Electrons', function() {
 
 	it('should return 0 for array [1, 2]', function() {
 		expect(electrons.calculate([1, 2])).toEqual(0);
-	});
-
-	it('should return 2 for array [1, 3]', function() {
-		expect(electrons.calculate([1, 3])).toEqual(2);
 	});
 });
