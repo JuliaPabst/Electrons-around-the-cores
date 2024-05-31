cf8f9e6741f6f086cef10c25113a828a43c0c6df
Jules <testadmin@Julias-MacBook-Pro-2.local>
2024-05-31 13:24:49 +0200

    create to do list: in spec.js

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -1,3 +1,17 @@
+/*
+TODO-LIST:
+- Create input and submit button
+- Read input value (string) and transform it to array: return array
+- get [1] as input - excpect 0 as output
+- get [3] as input - expect 2 as output
+- get [5] as input - expect 4 as output
+- get [1,2] as input - expect 0 as output
+- get [1,3] as input - expect 2 as output
+- get [3,5] as input - expect 6 as output
+
+*/
+
+
 describe('String Calculator', function() {
 	beforeEach(function() {
 		calculator = new StringCalculator();
