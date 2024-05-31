1aceeab3a5fddc406edc41e60c8f16c9330db771
Jules <testadmin@Julias-MacBook-Pro-2.local>
2024-05-31 13:37:30 +0200

    correct 2nd test: 1 - write failing test: Andrei Brate: observer, Julia Pabst: driver

--- string_calculator/spec/electrons_calculator_spec.js
+++ string_calculator/spec/electrons_calculator_spec.js
@@ -2,7 +2,6 @@
 
 TODO-LIST:
 - get [] as input - excpect 0 as output
-- get [1] as input - excpect 0 as output
 - get [3] as input - expect 2 as output
 - get [5] as input - expect 4 as output
 - get [1,2] as input - expect 0 as output
@@ -25,7 +24,7 @@ describe('Electrons', function() {
 		expect(electrons.calculate('')).toEqual(0);
 	});
 
-	it('should return 0 for string [1]', function() {
-		expect(electrons.calculate('1')).toEqual(0);
+	it('should return 0 for array [3]', function() {
+		expect(electrons.calculate([3])).toEqual(2);
 	});
 });
