3ca018a938406b549d1bdc2aa8cb471249d2b9f0
Jules <testadmin@Julias-MacBook-Pro-2.local>
2024-05-31 13:52:47 +0200

    3 - refactor:  Andrei Brate: observer, Julia Pabst: driver

--- string_calculator/src/electrons_calculator.js
+++ string_calculator/src/electrons_calculator.js
@@ -1,17 +1,17 @@
 // StringCalculator.js
 function ElectronsCalculator() {}
 ElectronsCalculator.prototype.calculate = function(array) {
-	if (array == []) {
-		return 0;
-	} else if(array[0] == 3) {
-		return 2;
-	} else if(array[0] == 5) {
-		return 4;
-	} else if(array[0] == 1 && array[1] == 2) {
-		 return 0;
+	let result = 0;
+for(let i = 0; i < array.length; i++) {
+	if(array[i] == 3) {
+		result += 2;
+	} else if(array[i] == 5) {
+		result += 4;
+	} else {
+		result += 0;
 	}
-	
-	return 0;
+}
+	return result;
 };
 
 
