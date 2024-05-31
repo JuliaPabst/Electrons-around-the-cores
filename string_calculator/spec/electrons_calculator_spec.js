e02ff7c67c0a2362017b19d4400edd7676efbf58
Jules <testadmin@Julias-MacBook-Pro-2.local>
2024-05-31 14:04:38 +0200

    1 - write failing test:  Andrei Brate: driver, Julia Pabst: observer

--- string_calculator/spec/electrons_calculator_spec.js
+++ string_calculator/spec/electrons_calculator_spec.js
@@ -8,7 +8,6 @@ if it is more than 6 - should return -2
 - get [3] as input - expect -1 as output
 - get [5] as input - expect -1 as output
 - get [1,2] as input - expect -1 as output
-- get [1,3] as input - expect -1 as output
 - get [1,2,3,4] as input - expect 2 as output
 - get [1,2,3,4,5,6,7] as input - expect -2 as output
 
@@ -28,19 +27,27 @@ describe('Electrons', function() {
 		electrons = new ElectronsCalculator(); 
 	});
 
-	it('should return 0 for an empty array', function() {
+	it('should return -1 for an empty array', function() {
 		expect(electrons.calculate('')).toEqual(-1);
 	});
 
-	it('should return 2 for array [3]', function() {
+	it('should return -1 for array [3]', function() {
 		expect(electrons.calculate([3])).toEqual(-1);
 	});
 
-	it('should return 4 for array [5]', function() {
+	it('should return -1 for array [5]', function() {
 		expect(electrons.calculate([5])).toEqual(-1);
 	});
 
-	it('should return 0 for array [1, 2]', function() {
+	it('should return -1 for array [1, 2]', function() {
 		expect(electrons.calculate([1, 2])).toEqual(-1);
+	});
+
+	it('should return 2 for array [1, 2, 3, 4]', function() {
+		expect(electrons.calculate([1, 2, 3, 4])).toEqual(2);
+	});
+
+	it('should return -2 for array [1, 2, 3, 4, 5, 6, 7]', function() {
+		expect(electrons.calculate([1, 2, 3, 4, 5, 6, 7])).toEqual(-2);
 	});
 });
